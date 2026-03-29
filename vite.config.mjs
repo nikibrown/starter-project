22fe9893a6a3086afd5921eda5566611ea7400d1
Niki Brown <284173+nikibrown@users.noreply.github.com>
2026-03-29 11:27:34 -0700

    tabs to spaces

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,24 +7,24 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const indexHtmlPath = path.resolve(__dirname, "src/index.html")
 
 export default defineConfig({
-	root: "src",
-	publicDir: path.resolve(__dirname, "public"),
-	build: {
-		outDir: path.resolve(__dirname, "docs"),
-		emptyOutDir: true,
-		rollupOptions: {
-			input: indexHtmlPath,
-		},
-	},
-	plugins: [
-		createHtmlPlugin({
-			entry: "/main.js",
-			inject: {
-				ejsOptions: {
-					// Required so EJS `include()` resolves paths relative to the page template
-					filename: indexHtmlPath,
-				},
-			},
-		}),
-	],
+  root: "src",
+  publicDir: path.resolve(__dirname, "public"),
+  build: {
+    outDir: path.resolve(__dirname, "docs"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: indexHtmlPath,
+    },
+  },
+  plugins: [
+    createHtmlPlugin({
+      entry: "/main.js",
+      inject: {
+        ejsOptions: {
+          // Required so EJS `include()` resolves paths relative to the page template
+          filename: indexHtmlPath,
+        },
+      },
+    }),
+  ],
 })
